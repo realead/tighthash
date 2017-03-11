c0a9c4d460e163d3a8f24133f84acc9852f28646
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2017-03-11 12:27:03 +0100

    introducint TightHashBase to cython version

--- tighthash/ctighthash.pyx
+++ tighthash/ctighthash.pyx
@@ -34,8 +34,8 @@ cdef class THSetIterator:
                 return self.__arr.data.as_ulongs[self.__it-2]
         
 
-cdef class TightHashSet:
-    cdef unsigned long long int __cnt
+cdef class TightHashBase:
+    cdef unsigned long long int cnt
     cdef double min_factor
     cdef double increase_factor
     cdef unsigned long long int size
@@ -44,39 +44,68 @@ cdef class TightHashSet:
     cdef long long int _add
     cdef array.array arr
     
-    
     def __init__(self,  capacity=1001, min_factor=1.2, increase_factor=1.2):
-      self.__cnt=0
-      self.min_factor=max(1.2, min_factor)
-      self.increase_factor= max(1.2, increase_factor)
-      self.size=self.ini_array(int(math.ceil(capacity*self.min_factor)));
-      self.contains_zero=0
-      self.mult=random.choice(_primes)
-      self._add=random.randint(100, 2000)
-    
-    cdef __move_pos(self, unsigned long long int pos):
-        return 0 if pos==self.size-1 else pos+1
-      
-    cdef __find(self, unsigned long long int start, unsigned long long int item):
-        while self.arr.data.as_ulongs[start] and self.arr.data.as_ulongs[start]!=item:
-            start=self.__move_pos(start)
-        return start 
-      
+        self.cnt=0
+        self.min_factor=max(1.2, min_factor)
+        self.increase_factor= max(1.2, increase_factor)
+        self.size=self.ini_array(int(math.ceil(capacity*self.min_factor)));
+        self.contains_zero=0
+        self.mult=random.choice(_primes)
+        self._add=random.randint(100, 2000)
+
     cdef ini_array(self, long long int minimal_size):
         self.arr=array.array('L', [0])
         array.resize(self.arr,minimal_size)
         array.zero(self.arr)
         return minimal_size
-            
+        
+    cdef move_pos(self, unsigned long long int pos):
+        return 0 if pos==self.size-1 else pos+1
+      
+    cdef find(self, unsigned long long int start, unsigned long long int item):
+        while self.arr.data.as_ulongs[start] and self.arr.data.as_ulongs[start]!=item:
+            start=self.move_pos(start)
+        return start 
+      
     cdef get_hash(self, unsigned long long int val):
         return (self.mult*hash(val)+self._add)%self.size
+               
+    def __iter__(self):
+        return THSetIterator(self.contains_zero, self.arr)      
+        
+    def __len__(self):     
+        return self.cnt+self.contains_zero
+        
+    def get_preallocated_size(self):
+        return len(self.arr)
+              
+
+    def __contains__(self, unsigned long long int val):
+        #the special case -> 0, in the array it means empty space
+        if not val:
+            return self.contains_zero
+        
+        #all values except 0:    
+        cdef unsigned long long int val_hash=self.get_hash(val)
+        val_hash=self.find(val_hash, val)   
+        
+        if self.arr.data.as_ulongs[val_hash]==val:
+            return True
+        return False
+
+cdef class TightHashSet(TightHashBase):
+    
+    def __init__(self,  capacity=1001, min_factor=1.2, increase_factor=1.2):
+        TightHashBase.__init__(self, capacity, min_factor, increase_factor)
+    
+
       
     cdef __realocate(self, unsigned long long int new_minimal_size):
         old_arr=self.arr
         
         self.size=self.ini_array(new_minimal_size)
         
-        self.__cnt=0
+        self.cnt=0
         for i in xrange(len(old_arr)):
             if old_arr.data.as_ulongs[i]!=0:
                     self.add(old_arr.data.as_ulongs[i])
@@ -91,40 +120,19 @@ cdef class TightHashSet:
                 return True
           
         #if there is not enough place -> reallocate   
-        if(self.__cnt*self.min_factor>self.size): 
+        if(self.cnt*self.min_factor>self.size): 
            self.__realocate(int(math.ceil(self.size*self.increase_factor)))
             
         
         #all values except 0:
         cdef unsigned long long int val_hash=self.get_hash(val)
-        val_hash=self.__find(val_hash, val)
+        val_hash=self.find(val_hash, val)
         if self.arr.data.as_ulongs[val_hash]:
             return False #already in the set
         
         self.arr.data.as_ulongs[val_hash]=val
-        self.__cnt+=1
+        self.cnt+=1
         return True
-        
-
-    def __contains__(self, unsigned long long int val):
-        #the special case -> 0, in the array it means empty space
-        if not val:
-            return self.contains_zero
-        
-        #all values except 0:    
-        cdef unsigned long long int val_hash=self.get_hash(val)
-        val_hash=self.__find(val_hash, val)   
-        
-        if self.arr.data.as_ulongs[val_hash]==val:
-            return True
-        return False
-        
-        
-    def __len__(self):     
-        return self.__cnt+self.contains_zero
-        
-    def get_preallocated_size(self):
-        return len(self.arr)
         
         
     def discard(self, unsigned long long int val):
@@ -133,26 +141,24 @@ cdef class TightHashSet:
                 self.contains_zero=0
              return
        cdef unsigned long long int val_hash=self.get_hash(val)  
-       cdef unsigned long long int pos=self.__find(val_hash, val)
+       cdef unsigned long long int pos=self.find(val_hash, val)
        if not self.arr.data.as_ulongs[pos]:
             return #not in the set!
             
        self.arr.data.as_ulongs[pos]=0 #delete
-       self.__cnt-=1
+       self.cnt-=1
         
        #reorder the next values
        #we a sure everything is OK only after meeting a 0
-       pos =self.__move_pos(pos)
+       pos =self.move_pos(pos)
        while self.arr.data.as_ulongs[pos]:
            val=self.arr.data.as_ulongs[pos]
-           self.__cnt-=1
+           self.cnt-=1
            self.arr.data.as_ulongs[pos]=0
            self.add(val)
-           pos=self.__move_pos(pos)
+           pos=self.move_pos(pos)
            
            
-    def __iter__(self):
-        return THSetIterator(self.contains_zero, self.arr)
         
   
         
